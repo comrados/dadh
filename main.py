e7784cf3b520f65512335347b4755a3b3f723b93
george <george@george.com>
2021-01-26 14:36:22 +0100

    req

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def train(**kwargs):
             loss_quant = i_ql + t_ql
             err = opt.alpha * weighted_cos_tri + opt.beta * loss_quant + opt.gamma * (loss_adver_feature + loss_adver_hash)
 
-            print((opt.alpha * weighted_cos_tri).cpu().detatch().numpy(), (opt.beta * loss_quant).cpu().detatch().numpy(), (opt.gamma * (loss_adver_feature + loss_adver_hash)).cpu().detatch().numpy())
+            print((opt.alpha * weighted_cos_tri).cpu().detach().numpy(), (opt.beta * loss_quant).cpu().detach().numpy(), (opt.gamma * (loss_adver_feature + loss_adver_hash)).cpu().detach().numpy())
 
             optimizer.zero_grad()
             err.backward()
