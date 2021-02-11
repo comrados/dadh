8d3e22098b757d65e63f1fd974001b52ce612dbd
george <georgii.mikriukov@tu-berlin.de>
2021-02-11 15:19:47 +0100

    .

--- utils.py
+++ utils.py
@@ -44,28 +44,40 @@ def image_from_numpy(x):
 
 
 def pr_curve(qB, rB, query_label, retrieval_label):
-    num_query = qB.shape[0]
-    num_bit = qB.shape[1]
-    P = torch.zeros(num_query, num_bit + 1)
-    R = torch.zeros(num_query, num_bit + 1)
+    num_query = qB.shape[0]  # length of query (each sample from query compared to retrieval samples)
+    num_bit = qB.shape[1]  # length of hash code
+    P = torch.zeros(num_query, num_bit + 1)  # precisions (for each sample)
+    R = torch.zeros(num_query, num_bit + 1)  # recalls (for each sample)
+
+    # for each sample from query calculate precision and recall
     for i in range(num_query):
+        # gnd[j] == 1 if same class, otherwise 0, ground truth
         gnd = (query_label[i].unsqueeze(0).mm(retrieval_label.t()) > 0).float().squeeze()
+        # tsum (TP + FN): total number of samples of the same class
         tsum = torch.sum(gnd)
         if tsum == 0:
             continue
-        hamm = calc_hamming_dist(qB[i, :], rB)
+        hamm = calc_hamming_dist(qB[i, :], rB)  # hamming distances from qB[i, :] (current query sample) to retrieval samples
+        # tmp[k,j] == 1 if hamming distance to retrieval sample j is less or equal to k (distance), 0 otherwise
         tmp = (hamm <= torch.arange(0, num_bit + 1).reshape(-1, 1).float().to(hamm.device)).float()
+        # total (TP + FP): total[k] is count of distances less or equal to k (from current query sample to retrieval samples)
         total = tmp.sum(dim=-1)
-        total = total + (total == 0).float() * 0.1
+        total = total + (total == 0).float() * 0.001  # replace zeros with 0.1 to avoid division by zero
+        # select only same class samples from tmp (ground truth masking, only rows where gnd == 1 proceed further)
         t = gnd * tmp
+        # count (TP): number of true (correctly selected) samples of the same class for any given distance k
         count = t.sum(dim=-1)
-        p = count / total
-        r = count / tsum
+        p = count / total  # TP / (TP + FP)
+        r = count / tsum  # TP / (TP + FN)
         P[i] = p
         R[i] = r
+    # mask to calculate P mean value (among all query samples)
     mask = (P > 0).float().sum(dim=0)
-    mask = mask + (mask == 0).float() * 0.1
+    mask = mask + (mask == 0).float() * 0.001
     P = P.sum(dim=0) / mask
+    # mask to calculate R mean value (among all query samples)
+    mask = (R > 0).float().sum(dim=0)
+    mask = mask + (mask == 0).float() * 0.001
     R = R.sum(dim=0) / mask
     return P, R
 
