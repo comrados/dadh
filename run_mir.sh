df302cf9ddd854369fba6c9429dae93b179f0142
george <georgii.mikriukov@tu-berlin.de>
2021-02-26 13:14:41 +0100

    .

--- run_mir.sh
+++ run_mir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py train --flag 'mir' --bit $*
+/home/george/Code/dadh/venv/bin/python main.py train --flag $1 --proc short --bit $2
---
+++ run_rsicd_short.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH -o /home/users/m/mikriukov/projects/dadh/out_gpu_short.log
+#SBATCH -J dadh
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --partition=gpu_short
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:tesla:1
+
+echo "Loading venv..."
+source /home/users/m/mikriukov/venvs/DADH/bin/activate
+
+echo "Loading cuda..."
+module load nvidia/cuda/10.1
+
+echo "Training 128..."
+python3 main.py train --flag ucm --proc short --bit 128
+
+echo "Testing 128..."
+python3 main.py test --flag ucm --proc short --bit 128
+
+echo "Training 64..."
+python3 main.py train --flag ucm --proc short --bit 64
+
+echo "Testing 64..."
+python3 main.py test --flag ucm --proc short --bit 64
+
+echo "Training 32..."
+python3 main.py train --flag ucm --proc short --bit 32
+
+echo "Testing 32..."
+python3 main.py test --flag ucm --proc short --bit 32
+
+echo "Training 16..."
+python3 main.py train --flag ucm --proc short --bit 16
+
+echo "Testing 16..."
+python3 main.py test --flag ucm --proc short --bit 16
+
+echo "Training 8..."
+python3 main.py train --flag ucm --proc short --bit 8
+
+echo "Testing 8..."
+python3 main.py test --flag ucm --proc short --bit 8
